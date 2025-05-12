ee08220d519fec55301ce93fb1ffc0463abcc053
BohdanLytvynov <blitvinov19@gmail.com>
2025-05-13 00:01:56 +0200

    Working on view

--- BarcodeReader.UI/ViewModels/Windows/MainWindowViewModel.cs
+++ BarcodeReader.UI/ViewModels/Windows/MainWindowViewModel.cs
@@ -4,16 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BarcodeReader.UI.ViewModels.Windows
 {
     internal class MainWindowViewModel : TitledViewModel
     {
         #region Fields
+        private string m_path;
 
+        private string m_result;
         #endregion
 
         #region Properties
+        public string PathToImg 
+        {
+            get=>m_path;
+            set=>Set(ref m_path, value);
+        }
+
+        public string Result 
+        {
+            get=>m_result;
+            set=>Set(ref m_result, value);
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand OnReadBarcodeButtonPressed { get; }
+
+        public ICommand OnClearButtonPressed { get; }
 
         #endregion
 
@@ -21,11 +42,20 @@ namespace BarcodeReader.UI.ViewModels.Windows
         public MainWindowViewModel()
         {
             SetTitle("Barcode Reader V 1.0.0.0");
+
+            m_path = string.Empty;
+            m_result = string.Empty;
         }
         #endregion
 
         #region Methods
+        #region On Read Button Pressed Execute
 
+        #endregion
+
+        #region On Clear Button Pressed Execute
+
+        #endregion
         #endregion
     }
 }
